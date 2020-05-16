ae66529aec5c562e558ec122c830bb6d35fb68d4
Cristian Esteban Martinez <cristianestebanmartinez888@gmail.com>
2020-05-16 18:13:35 -0300

    add file db-backup.json

--- src/App.js
+++ src/App.js
@@ -25,64 +25,7 @@ export default class App extends Component{
     super(props);
     this.state = {
       userName: 'Cristian',
-      products: [
-        {
-          id: 'prod01',
-          name: 'Notebook Samsung',
-          brand: 'Samsung',
-          price: 19000,
-          image:N1
-        },
-        {
-          id: 'prod02',
-          name: 'Notebook Samsung',
-          brand: 'Samsung',
-          price: 27000,
-          image:N2
-        },
-        {
-          id: 'prod03',
-          name: 'Notebook HP',
-          brand: 'HP',
-          price: 21000,
-          image:N3
-        },
-        {
-          id: 'prod04',
-          name: 'Notebook HP',
-          brand: 'HP',
-          price: 32000,
-          image:N4
-        },
-        {
-          id: 'prod05',
-          name: 'Notebook HP',
-          brand: 'HP',
-          price: 37000,
-          image:N5
-        },
-        {
-          id: 'prod06',
-          name: 'Notebook Dell',
-          brand: 'Dell',
-          price: 42000,
-          image:N6
-        },
-        {
-          id: 'prod07',
-          name: 'Notebook Dell',
-          brand: 'Dell',
-          price: 40000,
-          image:N7
-        },
-        {
-          id: 'prod08',
-          name: 'Notebook Macbook',
-          brand: 'Macbook',
-          price: 52000,
-          image:N8
-        }
-      ],
+      products: [],
       results:[],
       term:''
     }
