daca92a04c4a99b61e1975af0738464e601683a9
Cristian Esteban Martinez <cristianestebanmartinez888@gmail.com>
2020-06-22 18:48:34 -0300

    Configuration the login with Google and Faceboook

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import { Form, Input, Button, Checkbox, Card, Layout } from 'antd';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { GoogleLogin } from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
 
 const { Content, Header, Footer } = Layout;
 
 export class Login extends Component {
 
+    componentClicked = () => console.log("clicked");
     render() {
+        const responseGoogle = (response) => {
+            console.log(response);
+        }
+        const responseFacebook = (response) => {
+            console.log(response);
+        }
         return (
             <Layout>               
             <Header className='header'></Header>
@@ -40,7 +49,19 @@ export class Login extends Component {
                                 ]}>
                                 <Input.Password />
                             </Form.Item>
-
+                            <GoogleLogin
+                                clientId="354596986729-j2kjnq4ttrjh3fsffqi9lbar7ftt6r6o.apps.googleusercontent.com"
+                                buttonText="Sign in with Google"
+                                onSuccess={responseGoogle}
+                                onFailure={responseGoogle}
+                                cookiePolicy={'single_host_origin'}/>
+                            <FacebookLogin
+                                appId="288263259033402"
+                                autoLoad={true}
+                                fields="name,email,picture"
+                                onClick={this.componentClicked}
+                                callback={responseFacebook} 
+                                />
                             <Form.Item name="remember" valuePropName="checked">
                                 <Checkbox>Remember me</Checkbox>
                             </Form.Item>
